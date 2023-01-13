d1190255b3d0f10c9adf44161284a0285f3ea580
carlos1799 <carlosepila98@gmail.com>
2023-01-13 21:45:14 +0100

    Add: Function for getting a coordinate where there is a square aviable to pop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use rand::Rng;
 struct Game {
     start: bool,
@@ -23,32 +25,27 @@ impl Board {
         }
     }
     fn prepare_board(&mut self) {
-        
         for i in 0..self.matrix.len() {
             for z in 0..self.matrix[i].len() {
                 self.matrix[i][z].value = 0;
-
             }
         }
-        
+
         let mut rng = rand::thread_rng();
-        let mut i:i32 = rng.gen_range(0..4);
-        let mut z:i32 = rng.gen_range(0..4);
-
-
-
+        let mut i: i32 = rng.gen_range(0..4);
+        let mut z: i32 = rng.gen_range(0..4);
     }
-    
-    fn is_board_full(&self)->bool{
-      for i in 0..self.matrix.len() {
+
+    fn is_board_full(&self) -> bool {
+        for i in 0..self.matrix.len() {
             for z in 0..self.matrix[i].len() {
-                if self.matrix[i][z].value == 0{
-                     return false
+                if self.matrix[i][z].value == 0 {
+                    return false;
                 }
-       }
+            }
+        }
+        true
     }
-    true
-  }
     fn is_square_avaible(&self, x: usize, y: usize) -> bool {
         if self.matrix[x][y].value == 0 {
             true
@@ -56,10 +53,21 @@ impl Board {
             false
         }
     }
-    fn get_rnd_avaible_square(&self){
-        
-   
-
+    fn get_rnd_avaible_square(&self) -> Option<[usize; 2]> {
+        if self.is_board_full() {
+            None
+        } else {
+            let mut rng = rand::thread_rng();
+            let mut y: usize = rng.gen_range(0..4);
+            let mut x: usize = rng.gen_range(0..4);
+            while !self.is_square_avaible(x, y) {
+                println!("{},{}", x, y);
+                x = rng.gen_range(0..4);
+                y = rng.gen_range(0..4);
+            }
+            let coordinates: [usize; 2] = [x, y];
+            Some(coordinates)
+        }
     }
 }
 #[derive(Debug, Clone, Copy)]
@@ -93,15 +101,19 @@ impl Square {
 fn main() {
     let mut game = Game::new();
 
-    println!("{}",game.board.is_board_full());
+    println!("{}", game.board.is_board_full());
+    println!("{:?}", game.board.get_rnd_avaible_square());
+
     for i in 0..game.board.matrix.len() {
         for z in 0..game.board.matrix[i].len() {
             game.board.matrix[i][z].decide_number();
 
             print!("{:?}  ", game.board.matrix[i][z]);
+            println!("{:?}", game.board.get_rnd_avaible_square());
         }
         println!()
     }
-    println!("{}",game.board.is_board_full());
-    
+    println!("{}", game.board.is_board_full());
+
+    println!("{:?}", game.board.get_rnd_avaible_square());
 }
